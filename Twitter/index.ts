1f636b6da057886be584231e7ed2cb808d6e30ef
Shohei Kikuchi <killchance2256@gmail.com>
2022-11-23 22:48:41 +0900

    template

--- Twitter/index.ts
+++ Twitter/index.ts
@@ -1,2 +1,25 @@
 require('dotenv').config();
+const Twitter = require('twitter');
 
+
+
+const client = new Twitter({
+  consumer_key: process.env.APIKEY,
+  cosumer_secret: process.env.APIKEYSECRET,
+  access_token_key: process.env.ACCESSTOKEN,
+  access_token_secret: process.env.ACCESSTOKENSECRET
+});
+
+const message = "test tweet";
+
+client.get('favorites/list'), function({error, tweet, response}) {
+  if(error) throw error;
+  console.log(tweet);
+  console.log(response);
+}
+
+client.post('statuses/update', { status: message }, function({error, tweet, response}) {
+  if(error) throw error;
+  console.log(tweet);     
+  console.log(response);  
+});
